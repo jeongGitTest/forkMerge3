8afc1f2ca92f64622ae9f68e8bcd66af9f7f3cc0
JeongJH <ki660006@ack.co.kr>
2022-09-07 16:25:42 +0900

    first commit

---
+++ gittest/classA.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gittest
+{
+    class classA
+    {
+
+        private string msAutoClip = "";
+        private string msAutoClips1 = "";
+
+    }
+}
---
+++ gittest/classB.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gittest
+{
+    class classB
+    {
+
+        private string msAutoClip = "";
+        private string msAutoClips2 = "";
+        private string msAutoClips3 = "";
+    }
+}
