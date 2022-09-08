9ba38e0b942c50623b46732c2b370b4b7a308a53
JeongJH <ki660006@ack.co.kr>
2022-09-08 10:22:53 +0900

    feat: fork2 test

---
+++ gittest/classD.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class classD
+{
+    public string fork2_test = "";
+}
